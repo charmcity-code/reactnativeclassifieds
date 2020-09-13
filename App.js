08d266111f1562ce700e4cb871ce29a56681ba38
Mark Lawrence <lawrence.ml@gmail.com>
2020-09-12 20:18:03 -0400

    create login view

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import React, { useState, useEffect } from "react";
 import { SafeAreaView, View, Text } from "react-native";
+import Login from "./components/Login";
 
 export default function App() {
+  const doLogin = (email, password) => {
+    console.log(email, password);
+  };
+
   return (
     <>
       <SafeAreaView>
-        <View>
-          <Text>Hello world!</Text>
-        </View>
+        <Login doLogin={doLogin} />
       </SafeAreaView>
     </>
   );
